b5edfa15258eddf46eb00b616ac06c3bc5cd781d
James_Calvo <15jcalvo@gmail.com>
2022-02-11 14:09:41 -0500

    configured server

--- api/server.js
+++ api/server.js
@@ -6,4 +6,19 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    console.log('error')
+    res.status(err.status || 500).json({
+      message: `error: ${err.message}`,
+    })
+  })
+
 module.exports = server;

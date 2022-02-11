8594b4a88b5ee15f4a595c2746c97a28b01e3d8b
James_Calvo <15jcalvo@gmail.com>
2022-02-11 14:08:03 -0500

    actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,28 @@
 // add middlewares here related to actions
+const Actions = require('../actions/actions-model')
+
+function logger(req, res, next) {
+    console.log(req.method)
+    console.log(req.url)
+    console.log(Date.now())
+    next()
+  }
+
+async function checkActionId(req, res, next) {
+    try {
+        const possibleAction = await Actions.get(req.params.id)
+        if (possibleAction) {
+          req.action = possibleAction
+          next()
+        } else {
+          next({ status: 404, message: `No Action ${req.params.id}` })
+        }
+      } catch (err) {
+        next(err)
+      }
+}
+
+module.exports = {
+    logger,
+    checkActionId,
+}

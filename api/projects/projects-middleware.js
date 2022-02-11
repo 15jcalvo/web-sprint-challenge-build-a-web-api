e9b2bc8c5249e8bb94c5addd3c6ec757529fe080
James_Calvo <15jcalvo@gmail.com>
2022-02-11 14:12:42 -0500

    projects middleware added

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,28 @@
 // add middlewares here related to projects
+const Projects = require('../projects/projects-model')
+
+function logger(req, res, next) {
+    console.log(req.method)
+    console.log(req.url)
+    console.log(Date.now())
+    next()
+  }
+
+async function checkProjectId(req, res, next) {
+    try {
+        const possibleProject = await Projects.get(req.params.id)
+        if (possibleProject) {
+          req.project = possibleProject
+          next()
+        } else {
+          next({ status: 404, message: `No Project ${req.params.id}` })
+        }
+      } catch (err) {
+        next(err)
+      }
+}
+
+module.exports = {
+    logger,
+    checkProjectId,
+}
